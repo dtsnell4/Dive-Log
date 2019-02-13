cd28f78394a203265639d3eb77e33d805487a195
dtsnell4 <dtsnell4@yahoo.com>
2019-02-13 18:25:09 -0500

    Adding build of app to deploy to gh-pages

---
+++ build/precache-manifest.45ea1d9850342c359efccf50a75eb4d6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "98b2f65da13f755c8b86",
+    "url": "/static/css/main.cddd30a8.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "0c55e5112da0d31d8a42",
+    "url": "/static/js/1.0c55e511.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4a9e70672f224200238ed7fc6ed7ed10",
+    "url": "/static/media/sharks.4a9e7067.jpg"
+  },
+  {
+    "revision": "98b2f65da13f755c8b86",
+    "url": "/static/js/main.98b2f65d.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "0c55e5112da0d31d8a42",
+    "url": "/static/css/1.565edcb4.chunk.css"
+  },
+  {
+    "revision": "cd0fe8e0ac4b32a1f82df3875e0af0c1",
+    "url": "/index.html"
+  }
+];
